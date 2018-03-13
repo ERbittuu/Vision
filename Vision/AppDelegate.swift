e2452be197a07b0290fb06a9a8fba10df28da6e5
Utsav Patel(ERBittuu) <utsavhacker@gmail.com>
2018-03-13 23:27:05 +0530

    Initial Commit

---
+++ Vision/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Vision
+//
+//  Created by Utsav Patel on 13/03/18.
+//  Copyright © 2018 Utsav Patel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
